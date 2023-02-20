3d6b8e054e8d87b2c6a431aaf72e4a7762901ea2
JayO <45680293+ojhajayant@users.noreply.github.com>
2023-02-20 14:06:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,8 @@ def main_session_7_resnet():
     if args.use_albumentations:
         print(
             "Using albumentation lib for image-augmentation & other transforms")
-        train_dataset, test_dataset, train_loader, test_loader = \
+        train_dataset, test_dataset, train_loader, test_loader, \
+        lr_find_loader = \
             preprocess_albumentations.preprocess_data_albumentations(mean_tuple,
                                                                      std_tuple,
                                                                      img_size)
@@ -119,16 +120,14 @@ def main_session_7_resnet():
         init_weight_decay = weight_decay  # Based on L2 True/False
         end_lr = args.end_lr
         lr_range_test_epochs = args.lr_range_test_epochs
+
         best_lr = lr_finder.find_network_lr(model,
                                             criterion,
-                                            optimizer,
                                             device,
-                                            train_loader,
+                                            lr_find_loader,
                                             init_lr,
-                                            init_weight_decay,
                                             end_lr,
-                                            lr_range_test_epochs,
-                                            L1)
+                                            num_epochs=lr_range_test_epochs)
         print("best_lr is {}".format(best_lr))
     elif args.cmd == 'train':
         print("Model training starts on {} dataset".format(args.dataset))
@@ -139,7 +138,7 @@ def main_session_7_resnet():
         # from base_momentum of 0.85 to max_momentum of 0.95 during OCP cycle
         MOMENTUM = 0.9
         WEIGHT_DECAY = weight_decay
-        DIV_FACTOR = args.div_factor # default 10
+        DIV_FACTOR = args.div_factor  # default 10
         # final_div_factor = div_factor for NO annihilation
         FINAL_DIV_FACTOR = DIV_FACTOR
         EPOCHS = args.epochs
@@ -177,7 +176,8 @@ def main_session_7_resnet():
                         criterion,
                         scheduler=scheduler,
                         L1=L1)
-            model_name = test.test(model, device, test_loader, optimizer, epoch, criterion)
+            model_name = test.test(model, device, test_loader, optimizer, epoch,
+                                   criterion)
             last_best = last_best if (model_name == '') else model_name
         misc.plot_momentum_lr()
         misc.plot_acc()
@@ -197,9 +197,6 @@ def main_session_7_resnet():
                                  y_pred, test_dataset,
                                  title_str='Predicted Vs Actual')
         x_test = torch.from_numpy(x_test)
-        
-        print(
-            "The mis-classified images with GradCam, for this model are as shown below:")
 
         misc.show_gradcam_mislabelled(model, device, x_test,
                                       y_test.reshape(-1, 1), y_pred,
@@ -243,7 +240,8 @@ def main_session_8_custom_net():
     if args.use_albumentations:
         print(
             "Using albumentation lib for image-augmentation & other transforms")
-        train_dataset, test_dataset, train_loader, test_loader = \
+        train_dataset, test_dataset, train_loader, test_loader, \
+        lr_find_loader = \
             preprocess_albumentations.preprocess_data_albumentations(mean_tuple,
                                                                      std_tuple,
                                                                      img_size)
@@ -275,7 +273,7 @@ def main_session_8_custom_net():
     print(device)
 
     # Get the model loaded with summary(10 classes)
-    model = custom_resnet.custom_resnet(10).\
+    model = custom_resnet.custom_resnet(10). \
         to(device)
     if args.dataset == 'CIFAR10':
         summary(model, input_size=(3, 32, 32))
@@ -286,21 +284,16 @@ def main_session_8_custom_net():
         # Use LR-Range-Test to find maximum best LR as applicable to OCP
         # (best_lr)
         init_lr = args.init_lr
-        init_weight_decay = weight_decay  # Based on L2 True/False
         end_lr = args.end_lr
         lr_range_test_epochs = args.lr_range_test_epochs
-        
+
         best_lr = lr_finder.find_network_lr(model,
                                             criterion,
-                                            optimizer,
                                             device,
-                                            train_loader,
+                                            lr_find_loader,
                                             init_lr,
-                                            init_weight_decay,
                                             end_lr,
-                                            lr_range_test_epochs,
-                                            L1
-                                            )
+                                            num_epochs=lr_range_test_epochs)
         print("best_lr is {}".format(best_lr))
     elif args.cmd == 'train':
         print("Model training starts on {} dataset".format(args.dataset))
@@ -311,10 +304,10 @@ def main_session_8_custom_net():
         # from base_momentum of 0.85 to max_momentum of 0.95 during OCP cycle
         MOMENTUM = 0.9
         WEIGHT_DECAY = weight_decay
-        DIV_FACTOR = args.div_factor # default 10
+        DIV_FACTOR = args.div_factor  # default 10
         # final_div_factor = div_factor for NO annihilation
         FINAL_DIV_FACTOR = DIV_FACTOR
-        EPOCHS = args.epochs   # 24 here
+        EPOCHS = args.epochs  # 24 here
         MAX_LR_EPOCHS = args.max_lr_epochs  # 5 here
         NUM_OF_BATCHES = len(train_loader)
         PCT_START = MAX_LR_EPOCHS / EPOCHS
@@ -349,7 +342,8 @@ def main_session_8_custom_net():
                         criterion,
                         scheduler=scheduler,
                         L1=L1)
-            model_name = test.test(model, device, test_loader, optimizer, epoch, criterion)
+            model_name = test.test(model, device, test_loader, optimizer, epoch,
+                                   criterion)
             last_best = last_best if (model_name == '') else model_name
         misc.plot_momentum_lr()
         misc.plot_acc()
@@ -369,15 +363,13 @@ def main_session_8_custom_net():
                                  y_pred, test_dataset,
                                  title_str='Predicted Vs Actual')
         x_test = torch.from_numpy(x_test)
-        print(
-            "The mis-classified images with GradCam, for this model are as shown below:")
 
         misc.show_gradcam_mislabelled(model, device, x_test,
                                       y_test.reshape(-1, 1), y_pred,
                                       test_dataset, mean_tuple,
                                       std_tuple, layer='layer3')
 
-        
+
 if __name__ == '__main__':
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
